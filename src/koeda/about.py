7a525ef575aae1d7f314f396fc1cd631b3f7097e
toriving <toriving@gmail.com>
2020-11-25 00:26:23 +0900

    Add about this package information

---
+++ src/koeda/about.py
@@ -0,0 +1,21 @@
+__all__ = [
+    '__title__',
+    '__version__',
+    '__author__',
+    '__email__',
+    '__url__',
+    '__summary__',
+    '__license__',
+    '__copyright__'
+]
+
+__title__ = 'KoEDA'
+__version__ = '0.0.2'
+
+__author__ = 'Dongju Park'
+__email__ = 'toriving@gmail.com'
+__url__ = 'http://toriving.github.io'
+
+__summary__ = 'Easy Data Augmentation for Korean'
+__license__ = 'MIT License'
+__copyright__ = 'Copyright 2018 {}'.format(__author__)
