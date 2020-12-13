598bd14dc333ca2d2bc024ed5b1d6274ed5bf0e1
toriving <toriving@gmail.com>
2020-12-14 00:56:20 +0900

    Add list to string function for data augmentation

--- src/koeda/utils/__init__.py
+++ src/koeda/utils/__init__.py
@@ -1,5 +1,6 @@
-__all__ = ['WORDNET', 'STOPWORDS', 'get_synonyms']
+__all__ = ['WORDNET', 'STOPWORDS', 'get_synonyms', 'list_to_string']
 
 from .wordnet import *
 from .stopwords import *
 from .synonyms import *
+from .string import *
---
+++ src/koeda/utils/string.py
@@ -0,0 +1,2 @@
+def list_to_string(lst: list) -> str:
+    return " ".join(lst)
