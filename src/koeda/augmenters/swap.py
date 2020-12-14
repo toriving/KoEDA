28f6902b8fcf7671452d59c03dfc3503467643db
toriving <toriving@gmail.com>
2020-12-15 01:09:00 +0900

    Refactor swap function to class

--- src/koeda/augmenters/swap.py
+++ src/koeda/augmenters/swap.py
@@ -1,37 +1,58 @@
 import random
 from typing import Union
+from itertools import repeat
 
-from src.koeda.utils import list_to_string
-
-
-def random_swap(data: Union[list, str], p: float = 0.1) -> Union[list, str]:
-    if isinstance(data, str):
-        return _swap(data, p)
-    elif isinstance(data, list):
-        return list(map(_swap, data))
-    else:
-        raise Exception(f"Does not support the data type : {type(data)}")
+from src.koeda.utils import list_to_string, replace_space, revert_space, SPACE_TOKEN
+from konlpy.tag import *
 
 
-def _swap(data: str, p: float = 0.1) -> str:
-    words = data.split()
+class RandomSwap:
 
-    n = max(1, int(len(words) * p))
+    def __init__(self, morpheme_analyzer=None):
+        if morpheme_analyzer is None:
+            self.morpheme_analyzer = Okt()
+        elif morpheme_analyzer in ["Okt", "Kkma", "Komoran", "Mecab", "Hannanum"]:
+            self.morpheme_analyzer = eval(morpheme_analyzer)()
+        elif hasattr(morpheme_analyzer, "morphs"):
+            self.morpheme_analyzer = morpheme_analyzer
+        else:
+            raise Exception("Does not support morpheme analyzer.")
 
-    new_words = words.copy()
-    for _ in range(n):
-        new_words = swap_word(new_words)
-    return list_to_string(new_words)
+    def __call__(self, *args, **kwargs):
+        return self.random_swap(*args, **kwargs)
 
+    def random_swap(self, data: Union[list, str], p: float = 0.1) -> Union[list, str]:
+        if isinstance(data, str):
+            return self._swap(data, p)
+        elif isinstance(data, list):
+            return list(map(self._swap, data, repeat(p, len(data))))
+        else:
+            raise Exception(f"Does not support the data type : {type(data)}")
 
-def swap_word(new_words):
-    random_idx_1 = random.randint(0, len(new_words) - 1)
-    random_idx_2 = random_idx_1
-    counter = 0
-    while random_idx_2 == random_idx_1:
-        random_idx_2 = random.randint(0, len(new_words) - 1)
-        counter += 1
-        if counter > 3:
-            return new_words
-    new_words[random_idx_1], new_words[random_idx_2] = new_words[random_idx_2], new_words[random_idx_1]
-    return new_words
+    def _swap(self, data: str, p: float = 0.1) -> str:
+        split_words = self.morpheme_analyzer.morphs(replace_space(data))
+        words = self.morpheme_analyzer.morphs(data)
+
+        n = max(1, int(len(words) * p))
+
+        new_words = split_words.copy()
+        for _ in range(n):
+            new_words = self._swap_word(new_words)
+
+        print(words)
+        print(split_words)
+        print(new_words)
+        return revert_space(new_words)
+
+    def _swap_word(self, new_words):
+        random_idx_1 = random.randint(0, len(new_words) - 1)
+        random_idx_2 = random_idx_1
+        counter = 0
+        while random_idx_2 == random_idx_1:
+            random_idx_2 = random.randint(0, len(new_words) - 1)
+            counter += 1
+            if counter > 3:
+                return new_words
+        new_words[random_idx_1], new_words[random_idx_2] = new_words[random_idx_2], new_words[random_idx_1]
+
+        return new_words
