c0ac0b32f7c20097bf6cf3233725523500a8fbc2
toriving <toriving@gmail.com>
2020-12-17 01:41:43 +0900

    Add clean function to revert text function

--- src/koeda/utils/space.py
+++ src/koeda/utils/space.py
@@ -1,8 +1,13 @@
-SPACE_TOKEN = "___"
+SPACE_TOKEN = "\u241F"
 
 def replace_space(text: str) -> str:
     return text.replace(" ", SPACE_TOKEN)
 
 def revert_space(text: list) -> str:
-    return ' '.join(''.join(text).replace(SPACE_TOKEN, ' ').split())
-
+    clean = ' '.join(''.
+                   join(text).
+                   replace(SPACE_TOKEN, ' ').
+                   split()).\
+        replace(' .', '.').\
+        strip()
+    return clean
