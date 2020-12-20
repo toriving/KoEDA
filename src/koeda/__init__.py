170c5c9d5747719f57fa28411f43ecd84faf26d9
toriving <toriving@gmail.com>
2020-12-21 01:06:59 +0900

    Add package info

--- src/koeda/__init__.py
+++ src/koeda/__init__.py
@@ -1,3 +1,26 @@
-__all__ = ['EasyDataAugmentation']
+__title__ = 'KoEDA'
+__version__ = '0.0.3'
 
-from .eda import *
+__author__ = 'Dongju Park'
+__email__ = 'toriving@gmail.com'
+__url__ = 'http://toriving.github.io'
+
+__summary__ = 'Easy Data Augmentation for Korean'
+__license__ = 'MIT License'
+__copyright__ = 'Copyright 2020 {}'.format(__author__)
+
+
+from .eda import EasyDataAugmentation
+
+from .augmenters import (
+    RandomDeletion,
+    RandomInsertion,
+    SynonymReplacement,
+    RandomSwap
+)
+
+from .utils import (
+    STOPWORD,
+    WORDNET,
+    get_synonyms
+)
