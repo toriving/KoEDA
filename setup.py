921c22576e26b750afd21f710c31d411290d142d
toriving <toriving@gmail.com>
2020-12-21 01:07:27 +0900

    Update version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='koeda',
-    version='0.0.2',
+    version='0.0.3',
     description='Korean Easy Data Augmentation Package',
     long_description=open("README.md", "r", encoding="utf-8").read(),
     long_description_content_type="text/markdown",
